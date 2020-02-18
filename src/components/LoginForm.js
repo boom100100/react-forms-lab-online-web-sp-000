4f17e0db34bcd572985877fb57a638a80062f2bd
Bernadette Davis <bsd231@nyu.edu>
2020-02-18 23:00:20 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 
 class LoginForm extends React.Component {
@@ -48,3 +49,55 @@ class LoginForm extends React.Component {
 }
 
 export default LoginForm;
+=======
+import React from "react";
+
+class LoginForm extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+  
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+  
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (!this.state.username || !this.state.password) return;
+    this.props.handleLogin(this.state);
+  }
+
+  render() {
+    return (
+      <div>
+      <form onSubmit={this.handleSubmit} >
+        <div>
+          <label>
+            Username
+            <input id="username" name="username" type="text" onChange={this.handleChange} value={this.state.username} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Password
+            <input id="password" name="password" type="password" onChange={this.handleChange} value={this.state.password} />
+          </label>
+        </div>
+        <div>
+          <button type="submit" >Log in</button>
+        </div>
+      </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
+>>>>>>> bbcd9980d7cb33bc523da41db6b6c99a21b63f24
